a42fa1e2c8ec8148481a3c309f73772946e63ffb
peivol-dot-r <roktarofficial@gmail.com>
2020-08-31 11:44:56 +0100

    pacman finished

--- script.js
+++ script.js
@@ -1,108 +1,137 @@
-
+////////////////////
 /* Create a Ghosts*/
+////////////////////
 function ghost(id,speed,loction){
         this.type = "Ghost";
         this.GhostID = id;
         this.speed = speed;
+        this.initialLocation = loction;
         this.loction = loction;
         this.moveTo = (dir,lction,playboard) => {
-            if(playboard.childNodes[lction + dir].className != "ghost"){
-                if(map[dir + lction] == 0 ){ // empty board cell 
-                    playboard.childNodes[lction + dir].className = this.GhostID; 
-                    playboard.childNodes[lction].className = "boardCell";
-                    this.loction = lction + dir;  
-                }else if (map[dir + lction] == 1){ // pointcell
-                    playboard.childNodes[lction + dir].className = this.GhostID; 
-                    playboard.childNodes[lction].className = "pointsCell";
-                    this.loction = lction + dir;
-                }else if(map[dir + lction] == 3){
-                    playboard.childNodes[lction + dir].className = this.GhostID; 
-                    playboard.childNodes[lction].className = "ghostLair";
-                    this.loction = lction + dir;
-                }else{
-                    this.autoMove();
+            if(playboard.childNodes[lction + dir].className == "pacmanPosition"){
+                document.getElementById("lose").style.display = "block";
+                game.playing = 0;
+                pacman.loction = -1;
+
+            }else if(playboard.childNodes[lction + dir].className != "ghost" && playboard.childNodes[lction + dir].className != "wall"){
+                switch(map[lction]){
+                    case 0: // empty
+                        playboard.childNodes[lction + dir].className = this.GhostID; 
+                        playboard.childNodes[lction].className = "boardCell";
+                        this.loction = lction + dir;
+                    break
+                    case 1: // point
+                        playboard.childNodes[lction + dir].className = this.GhostID; 
+                        playboard.childNodes[lction].className = "pointsCell";
+                        this.loction = lction + dir;
+                    break
+                    case 3: // ghostlair
+                        playboard.childNodes[lction + dir].className = this.GhostID; 
+                        playboard.childNodes[lction].className = "ghostLair";
+                        this.loction = lction + dir;
+                    break
                 }
             }
         };
         this.autoMove = () => {
-            var playboard = document.getElementById("playBoard");
-            let ind = this.loction;
-            let moveDirection = Math.floor(Math.random()*4+1);
-            switch(moveDirection){
-                case 0: // stop
+            if(game.playing == 1){
+                var playboard = document.getElementById("playBoard");
+                let ind = this.loction;
+                let moveDirection = Math.floor(Math.random()*4+1);
+                switch(moveDirection){
+                    case 0: // stop
+                    break
+                    case 1: // up
+                        this.moveTo(-24,ind,playboard);
+                    break
+                    case 2: // down
+                        this.moveTo(24,ind,playboard);
+                    break
+                    case 3: // left
+                    if((ind -1) == 119){
+                        this.moveTo(23,ind,playboard);
+                    } else{
+                        this.moveTo(-1,ind,playboard);
+                    }
                 break
-                case 1: // up
-                    this.moveTo(-24,ind,playboard);
+                case 4: // right
+                    if((ind + 1) == 144){
+                        this.moveTo(-23,ind,playboard);
+                    } else{
+                        this.moveTo(1,ind,playboard);
+                    }
                 break
-                case 2: // down
-                    this.moveTo(24,ind,playboard);
-                break
-                case 3: // left
-                if((ind -1) == 119){
-                    this.moveTo(23,ind,playboard);
-                } else{
-                    this.moveTo(-1,ind,playboard);
                 }
-            break
-            case 4: // right
-                if((ind + 1) == 144){
-                    this.moveTo(-23,ind,playboard);
-                } else{
-                    this.moveTo(1,ind,playboard);
-                }
-            break
-            } 
+            }
         };
-    }
-
+}
+//////////////////
 /* Pacman object*/
-
+//////////////////
 var pacman = {
     type: "pacman",
     moveDirection: 0,
     loction: 251,
-    speed:500,
+    speed:250,
     autoMove:function(){
-        let playboard = document.getElementById("playBoard");
-        var ind = pacman.loction;
-        switch(pacman.moveDirection){
-            case 0: // stop
-            break
-            case 1: // up
-                pacman.moveTo(-24,ind,playboard);
-            break
-            case 2: // down
-                pacman.moveTo(24,ind,playboard);
-            break
-            case 3: // left
-                if((ind -1) == 119){
-                    console.log(ind - 1)
-                    pacman.moveTo(23,ind,playboard);
-                } else{
-                    pacman.moveTo(-1,ind,playboard);
-                }
-            break
-            case 4: // right
-                if((ind + 1) == 144){
-                    console.log(ind + 1)
-                    pacman.moveTo(-23,ind,playboard);
-                } else{
-                    pacman.moveTo(1,ind,playboard);
-                }
-            break
-        } 
+        if(game.playing == 1){
+            let playboard = document.getElementById("playBoard");
+            var ind = pacman.loction;
+            switch(pacman.moveDirection){
+                case 0: // stop
+                break
+                case 1: // up
+                    pacman.moveTo(-24,ind,playboard);
+                break
+                case 2: // down
+                    pacman.moveTo(24,ind,playboard);
+                break
+                case 3: // left
+                    if((ind -1) == 119){ // border move
+                        pacman.moveTo(23,ind,playboard);
+                    } else{
+                        pacman.moveTo(-1,ind,playboard);
+                    }
+                break
+                case 4: // right
+                    if((ind + 1) == 144){ // border move
+                        pacman.moveTo(-23,ind,playboard);
+                    } else{
+                        pacman.moveTo(1,ind,playboard);
+                    }
+                break
+            }
+            var countCells = 0;
+            for(var i = 0; i < map.length; ++i){
+                if(map[i] == 1)
+                    countCells++;
+            }
+            if(countCells == 0){
+                document.getElementById("win").style.display = "block";
+                game.playing = 0;
+                pacman.loction = -1;
+            }
+        }
     },
     moveTo: function(dir,lction,playboard){
-        if(map[dir + lction] == 0 ){ // empty board cell 
-            playboard.childNodes[lction + dir].className = "pacmanPosition";
-            playboard.childNodes[lction].className = "boardCell";
-            pacman.loction = lction + dir;
-        }else if (map[dir + lction] == 1){ // pointcell
-            playboard.childNodes[lction + dir].className = "pacmanPosition";
-            playboard.childNodes[lction].className = "boardCell";
-            pacman.loction = lction + dir;
+        if( playboard.childNodes[lction + dir].className == "ghost"){
+            document.getElementById("lose").style.display = "block";
+            game.playing = 0;
+            pacman.loction = -1;
         }else{
-            pacman.moveDirection= 0;
+            if(map[dir + lction] == 0 ){ // empty board cell 
+                playboard.childNodes[lction + dir].className = "pacmanPosition";
+                playboard.childNodes[lction].className = "boardCell";
+                pacman.loction = lction + dir;
+            }else if (map[dir + lction] == 1){ // pointcell
+                game.points = game.points +10;
+                playboard.childNodes[lction + dir].className = "pacmanPosition";
+                playboard.childNodes[lction].className = "boardCell";
+                map[dir + lction] = 0;
+                pacman.loction = lction + dir;
+            }else{
+                pacman.moveDirection= 0;
+            }
         }
     },
     pressKey: function(keyInp){
@@ -131,14 +160,14 @@ var pacman = {
             break
             case 68: // right with D character
                 pacman.moveDirection = 4;
-                
             break
         }
     }
 } 
-
+//////////////////////////
 /* Create the playboard */
-var map = {
+//////////////////////////
+var mapCreate = {
     type: "map",
     inputCell: function(cell){
         const newDiv = document.createElement("div"); // create a new div element 
@@ -146,7 +175,6 @@ var map = {
         playBoard.appendChild(newDiv);  // add cell to playboard
     },
     createMap: function(){
-        window.map = this.mapTemplate;
         for (var i = 0; i < this.mapTemplate.length; i++) {  //input the map
             switch(this.mapTemplate[i]){ // Apply move function bassed on keybord input
                 case 0: 
@@ -181,17 +209,42 @@ var map = {
                     2,0,0,1,1,1,1,1,1,2,0,0,0,2,1,1,1,1,1,1,1,0,0,2,
                     2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
 } 
+///////////////
+/* Game State*/
+///////////////
+var game = {
+    type: "state",
+    playing: 0,
+    gameOver: 0,
+    points:0,
+    pointsLeft: 0,
+    play: function(){
+        this.playing = 1;
+        document.getElementById("stop").style.display = "none";
+      },
+    pause: function(){
+        this.playing = 0;
+        document.getElementById("stop").style.display = "block";
+    },
+    reset: function(){
+        location.reload();
+    },
+    displayPoints: function(){
+        document.getElementById("points").innerHTML = game.points;
+    }
+}
 
 
-
-
+////////////////////////
 /* Creating setting */
-map.createMap(); // create map
+//////////////////////////
+mapCreate.createMap(); // create map
+var map = mapCreate.mapTemplate;
 ghosts =[ // create mobs
-    new ghost("ghost",500,131),
-    new ghost("ghost",450,132),
-    new ghost("ghost",300,153),
-    new ghost("ghost",250,157),
+    new ghost("ghost",250,131),
+    new ghost("ghost",300,132),
+    new ghost("ghost",350,153),
+    new ghost("ghost",200,157),
 ]
 
 document.getElementById("playBoard").childNodes[251].className = "pacmanPosition"; // add packman to map
@@ -199,67 +252,13 @@ for(var i = 0; i< ghosts.length; i++){ // add ghosts to map
     document.getElementById("playBoard").childNodes[ghosts[i].loction].className = ghosts[i].GhostID;
 }
 
-/* Movement */
-document.onkeydown = pacman.pressKey;  // Pacman moves 
-setInterval(pacman.autoMove, pacman.speed);
-
+//////////////////////
+/* Moving Creatures */
+//////////////////////
+document.onkeydown = pacman.pressKey;  // Pacman key press 
+setInterval(pacman.autoMove, pacman.speed); // move packman
+setInterval(game.displayPoints, pacman.speed); // update points 
 for(var i = 0; i< ghosts.length; i++){ // Ghosts moves
     setInterval(ghosts[i].autoMove,  ghosts[i].speed);
 }
-
-
-
-
-
-
-
-
-
-
-    
-
-/*  Move Overlay
-function checkKey(keyInp) {
-    keyInp = keyInp || window.event;
-    var playboard = document.getElementById("playBoard");
-    var ind = map.indexOf(4);
-
-    function moveTo(direction) {  // function to move packman to another cell
-        if(map[ind+direction] == 1){
-            let stop = 1;
-        } else {
-            playboard.childNodes[ind + direction].className = "packman";
-            map[ind+direction] = 4;
-            playboard.childNodes[ind].className = "boardCell";
-            map[ind] = 0;
-        }
-    }
-    if (keyInp.keyCode == '38') {// Apply move function bassed on keybord input
-        let packman = document.getElementsByClassName("packman");
-        let dir = packman[0].offsetTop;
-        console.log()
-        let newDir = dir - 5;
-        packman[0].style.top = newDir.toString().concat("px");
-    }else if (keyInp.keyCode == '40') { // down arrow
-        let packman = document.getElementsByClassName("packman");
-        let dir = packman[0].offsetTop;
-        let newDir = dir + 5;
-        packman[0].style.top = newDir.toString().concat("px");
-        console.log(newDir)
-    }else if (keyInp.keyCode == '37') {// left arrow
-        let packman = document.getElementsByClassName("packman");
-        let dir = packman[0].offsetLeft;
-        let newDir = dir - 5;
-        packman[0].style.left = newDir.toString().concat("px");
-        console.log(newDir)
-    }else if (keyInp.keyCode == '39') {// right arrow
-        let packman = document.getElementsByClassName("packman");
-        let dir = packman[0].offsetLeft;
-        let newDir = dir + 5;
-        packman[0].style.left = newDir.toString().concat("px");
-    }
-}
-
-
-*/
 
